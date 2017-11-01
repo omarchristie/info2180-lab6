87e920bd7db4b78657e7ed52b3df0b8e126d514a
Omar Christie <omar.christie@mymona.uwi.edu>
2017-11-01 18:13:43 +0000

    EX3

--- definition.js
+++ definition.js
@@ -1,3 +1,4 @@
+/* global $ */ 
 $(document).ready(function(){
 	$('#search').click(function(){
 		$.ajax({
@@ -7,6 +8,25 @@ $(document).ready(function(){
 			success: function(data){
 				$('#result').html(data);
 			}
+		})
+	})
+	
+	$('#searchall').click(function(){
+		$.ajax({
+			type: 'GET',
+			url: 'request.php',
+			data:{q: '&all=true'},
+			success: function(data){
+				$('#result').html("");
+				$("#result").append('<ol></ol>');
+				$(data).find('definition').each(function(){
+					var Response = '<h3>'+$(this).find("name").text()+'</h3>';
+					Response += '<p>'+$(this).find("meaning").text()+'</p>';
+					Response += '<p>'+"-"+$(this).find("author").text()+'</p>';
+					$("#result ol").append('<li>'+Response+'</li>');
+				})
+			}
+			
 		})
 	})
 })
